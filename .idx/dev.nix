ca5641c250dd5fdf9808e26ad3ec01c3036ce3e4
yoga <t48189588@gmail.com>
2026-08-03 16:07:51 +0900

    Deploy 2026-08-03 16:07:51

--- .idx/dev.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: {
-  channel = "stable";
-  packages = [ pkgs.flutter pkgs.jdk17 ];
-  idx.extensions = [ "Dart-Code.flutter" ];
-  idx.workspace.onCreate = {
-    pub-get = "flutter pub get";
-  };
-}
--- sharepoint_reservation_app/README.md
+++
@@ -1,2 +0,0 @@
-# External form for reservation purposes
-This is flutter web form that would allow external users to request reservations to Global Communication Lounge (GCL).
